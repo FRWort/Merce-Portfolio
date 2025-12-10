c0232d6b8e4a37bd46fabab0ec841009fd00ea89
Francisco Wortman <66385328+FRWort@users.noreply.github.com>
2025-12-10 16:03:45 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -16,18 +16,6 @@ const scroll = new LocomotiveScroll ({
     },
 });
 
-// Useful function for sticky navbars with Locomotive scroll
-const navBar = document.querySelector("header");
-
-scroll.on("scroll", (position) => {
-    
-    if (position.scroll.y > 500) {
-      navBar.classList.add("secondary-nav");
-    } else {
-      navBar.classList.remove("secondary-nav");
-
-    }
-});
 
 $(".menu").click(function(){
   $(this).parent().toggleClass("close");
@@ -65,22 +53,22 @@ $(document).ready(function () {
   $(".paints img").each(function () {
     var img = $(this);
     var overlay = $('<div class="hover-overlay"><p>VER CATÁLOGO</p></div>');
-
     // Append overlay to the image's parent (e.g., a container div)
     img.parent().css('position', 'relative').append(overlay);
-
     // Hide overlay initially
     overlay.hide();
-
-    // Show on hover
-    img.hover(
-      function () {
-        overlay.show();
-      },
-      function () {
-        overlay.hide();
-      }
-    );
+    // Only add hover functionality on screens wider than 900px (desktop/tablet landscape)
+    if (window.matchMedia('(min-width: 1080px)').matches) {
+      // Show on hover
+      img.hover(
+        function () {
+          overlay.show();
+        },
+        function () {
+          overlay.hide();
+        }
+      );
+    }
   });
   /* VER CATALOGO END */
 
@@ -96,11 +84,11 @@ $(document).ready(function () {
       margin: 0,
       autoplay: true,
       responsiveClass: true,
-      nav: true,
+      nav: false,
       responsive: {
         0: {
           items: 1,
-          nav: true
+          nav: false
         },
         800: {
           items: 3,
@@ -115,6 +103,8 @@ $(document).ready(function () {
   })(jQuery);
 })();
 
+/* BUTTONS */
+
 document.querySelector('.violet-button').addEventListener('click', function () {
   window.location.href = 'https://api.whatsapp.com/send?phone=5492645455759&text=%F0%9F%96%90%EF%B8%8F%20Hola!%20Te%20hablo%20a%20trav%C3%A9s%20de%20tu%20website!%20Tengo%20una%20consulta';
 });
@@ -123,47 +113,12 @@ document.querySelector('.murals-button').addEventListener('click', function () {
   window.location.href = 'murales.html';
 });
 
-document.querySelector('.paints').addEventListener('click', function () {
-  window.location.href = 'pinturas.html';
-});
-
-/*
-// Fixed with https://bobbyhadz.com/blog/javascript-getboundingclientrect-is-not-a-function
-function isElementVisible(el) {
-  var rect     = el.getBoundingClientRect(),
-      vWidth   = window.innerWidth || document.documentElement.clientWidth,
-      vHeight  = window.innerHeight || document.documentElement.clientHeight,
-      efp      = function (x, y) { return document.elementFromPoint(x, y) };     
-
-  // Return false if it's not in the viewport
-  if (rect.right < 0 || rect.bottom < 0 
-          || rect.left > vWidth || rect.top > vHeight)
-      return false;
-
-  // Return true if any of its four corners are visible
-  return (
-        el.contains(efp(rect.left,  rect.top))
-    ||  el.contains(efp(rect.right, rect.top))
-    ||  el.contains(efp(rect.right, rect.bottom))
-    ||  el.contains(efp(rect.left,  rect.bottom))
-  );
+if (window.matchMedia('(min-width: 1080px)').matches) {
+  document.querySelector('.paints').addEventListener('click', function () {
+    window.location.href = 'pinturas.html';
+  });
 }
 
-const eledetected = document.getElementsByClassName("detect");
-
-// Change on scroll -> Search for other function (when not scrolled, not detected) DOMContentLoaded??
-scroll.on('scroll', (obj) => {
-
-  var elements = document.getElementsByClassName("detect");
-  for (i = 0; i < elements.length; i++) {
-    if (isElementVisible(elements[i])) {
-      elements[i].classList.add("detected");
-      console.log("Added");
-    } else {
-      elements[i].classList.remove("detected");
-      console.log("Removed");
-    }
-  }
-  
+document.querySelector('.paintings-button').addEventListener('click', function () {
+  window.location.href = 'pinturas.html';
 });
-*/
